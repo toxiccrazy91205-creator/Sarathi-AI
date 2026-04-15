c21e2d5c41fb91c152742c5b2fe158c1fa9b5128
Sarathi-AI26 <harshendras0@gmail.com>
2026-04-15 19:06:24 +0530

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,1 +1,7 @@
-
+export default function Footer() {
+  return (
+    <footer style={{ padding: '20px', borderTop: '1px solid #eaeaea', textAlign: 'center', marginTop: '50px' }}>
+      <p>© 2026 Sarthi AI Platform | Empowering Careers in India</p>
+    </footer>
+  );
+}
